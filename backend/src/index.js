b6fb2b5f3716ef300f59e869342480e9ecf23a8c
Ayush Jha <jhaaayush303@gmail.com>
2026-07-05 00:29:38 +0530

    modified few files to set the base URL for API requests and socket connections based on the environment (development or production). Updated CORS settings in the backend to allow requests from the appropriate origin.

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,10 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.CLIENT_URL
+        : "http://localhost:5173",
     credentials: true,
   }),
 );
